6773140ccadf1ea7e97485ae25d7ae216b27d831
mbelotti-nuk <75174915+mbelotti-nuk@users.noreply.github.com>
2023-12-03 11:07:14 +0100

    change loss

--- pkdnn/exportmodel.py
+++ pkdnn/exportmodel.py
@@ -8,6 +8,20 @@ from .export import exportsd
 import os
 from .net.pk_nn import pknn
 from .functionalities.config import load_config
+import pickle
+
+def write_scale(scaler):
+    frmt = '{0:<20} {1:<10} {2:>20} {3:>20}\n'
+    lines = frmt.format("input", "scale type" , "val 1", "val 2")
+    lines += "\n"
+    for key, val in scaler.inp_scale_1.items():
+        lines += frmt.format(key, scaler.input_scale_type[key], val, scaler.inp_scale_2[key])
+    lines += "\n\n\n"
+    lines += frmt.format("output", "scale type" , "val 1", "val 2")
+    lines += "\n"
+    out_type = scaler.out_key
+    lines += frmt.format( out_type, scaler.output_scale_type[out_type], scaler.out_scale_1, scaler.out_scale_2)
+    return lines
 
 def main():
     try:
@@ -24,7 +38,13 @@ def main():
         print(e)
         print("Failed to create NN model")
 
-    
+
+    scaler = pickle.load(open(config['io_paths']['path_to_scaler'], "rb", -1))
+    fout = open(os.path.join(config['io_paths']["save_path"], "Specifics_scaler.txt"),"w")
+    fout.write(write_scale(scaler))
+    fout.close()
+
+
     try:
         f = open(os.path.join(config['io_paths']["save_path"],config['io_paths']["save_name"]+".dat"), "wb")
         exportsd.save_state_dict(model.to("cpu").state_dict(), f)
